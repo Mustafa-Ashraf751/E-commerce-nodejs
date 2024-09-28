bbfd1f1ab86ba49ffec6f916e584ef12237ced76
Mustafa-Ashraf <mustafaashraf123@yahoo.com>
2024-09-29 00:42:36 +0300

    Make custom errors
    Make custom errors to use them in Global middleware handler

---
+++ errors/index.js
@@ -0,0 +1,8 @@
+const customAPIError = require("./custom-api");
+const BadRequestError = require("./badRequest");
+const NotFoundError = require("./not-found");
+module.exports = {
+  customAPIError,
+  BadRequestError,
+  NotFoundError,
+};
