0d5058a17b9590559fafd94bb30e4121092cf43e
Mustafa-Ashraf <mustafaashraf123@yahoo.com>
2024-09-28 15:07:54 +0300

    Create usermodel
    Create userModel to map object to database schema

---
+++ models/UserModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name!"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    unique: true,
+
+    validator: {
+      validator: validator.isEmail,
+      Message: "Please provide valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
